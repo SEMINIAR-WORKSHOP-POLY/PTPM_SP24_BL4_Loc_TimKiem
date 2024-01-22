5b597cd726da98ace2095a2c3050137cdd97d0f8
HangNT169 <104345338+HangNT169@users.noreply.github.com>
2024-01-22 11:39:57 +0700

    Add files via upload

---
+++ 1_WS_ECE_Search.sql
@@ -0,0 +1,12 @@
+﻿-- DEMO SEARCH (Tìm kiếm một từ) 
+DECLARE @input NVARCHAR(MAX) = '%ce%'
+
+SELECT * FROM san_pham
+	WHERE ten_san_pham LIKE @input
+		OR danh_muc LIKE @input
+		OR mat_hang LIKE @input
+		OR phan_loai LIKE @input
+		OR mo_ta LIKE @input
+		OR chat_lieu LIKE @input
+		OR don_gia LIKE @input
+		OR so_luong LIKE @input
---
+++ 2_WS_ECE_Filter.sql
@@ -0,0 +1,15 @@
+﻿-- DEMO FILTER
+DECLARE @tensanpham NVARCHAR(MAX) = NULL;
+DECLARE @danhmuc NVARCHAR(MAX) = NULL;
+DECLARE @mat_hang NVARCHAR(MAX) = NULL;
+DECLARE @mo_ta NVARCHAR(MAX) = NULL;
+DECLARE @chat_lieu NVARCHAR(MAX) = NULL;
+
+SELECT *
+FROM san_pham
+WHERE
+    (ten_san_pham LIKE '%' + @SearchTerm_tensanpham + '%' OR @SearchTerm_tensanpham IS NULL)
+    AND (danh_muc LIKE '%' + @SearchTerm_danhmuc + '%' OR @SearchTerm_danhmuc IS NULL)
+    AND (mat_hang LIKE '%' + @SearchTerm_mat_hang + '%' OR @SearchTerm_mat_hang IS NULL)
+    AND (mo_ta LIKE '%' + @SearchTerm_mo_ta + '%' OR @SearchTerm_mo_ta IS NULL)
+    AND (chat_lieu LIKE '%' + @SearchTerm_chat_lieu + '%' OR @SearchTerm_chat_lieu IS NULL);
